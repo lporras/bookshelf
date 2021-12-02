6f65fb723469bdfef2c252f8ce7582dc993515e2
Luis Alfredo Porras Páez <lporras16@gmail.com>
2021-12-02 10:56:39 -0500

    update and delete author

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -2,6 +2,7 @@ class Types::AuthorInputType < GraphQL::Schema::InputObject
   graphql_name "AuthorInputType"
   description "All the attributes for creating an author"
 
+  argument :id, ID, required: false
   argument :first_name, String, required: false, camelize: false
   argument :last_name, String, required: false, camelize: false
   argument :yob, Int, required: false
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -13,4 +13,21 @@ class Types::MutationType < Types::BaseObject
 
   field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
 
+  field :update_author, Boolean, null: false, description: "Update an author" do
+    argument :author, Types::AuthorInputType, required: true
+  end
+
+  def update_author(author:)
+    existing = Author.where(id: author[:id]).first
+    existing&.update author.to_h
+  end
+
+  field :delete_author, Boolean, null: false, description: "Delete an author" do
+    argument :id, ID, required: true
+  end
+
+  def delete_author(id:)
+    Author.where(id: id).destroy_all
+    true
+  end
 end
